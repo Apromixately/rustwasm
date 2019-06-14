83ce95500d46d1c6abe02d65bdfd625ddc74527e
Apromixately <Apromixately@users.noreply.github.com>
2019-06-14 23:31:28 +0200

    Add broken fire.

--- fire/src/lib.rs
+++ fire/src/lib.rs
@@ -1,4 +1,11 @@
 use wasm_bindgen::prelude::*;
+extern crate console_error_panic_hook;
+
+#[wasm_bindgen]
+extern {
+#[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 #[wasm_bindgen]
 pub struct Canvas {
@@ -6,6 +13,7 @@ pub struct Canvas {
     height: u32,
     buf: Vec<u8>,
     t: u32,
+    oldt: u32,
 }
 
 #[wasm_bindgen]
@@ -27,23 +35,53 @@ impl Canvas {
     }
 
     pub fn draw(&mut self) {
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let i = (row * self.width + col) as usize;
+        //if self.t < self.oldt + 1000 {
+        //    return;
+        //}
+        self.oldt = self.t;
+        for row in 1..self.height {
+            for col in 5..self.width-5 {
+                let offset: u32 = if js_sys::Math::random() < 0.5 { 1 } else { self.width-1 };
+                let newcol = (col + offset) % self.width;
 
-                self.buf[4 * i]     = (row ^ col) as u8; // r
-                self.buf[4 * i + 1] = ((self.t/10+row) ^ ((self.t / 100)+col)) as u8; // g
-                self.buf[4 * i + 2] = (self.t / 10000) as u8; // b
-                self.buf[4 * i + 3] = 255 as u8; // alpha
+                let i = (row * self.width + col) as usize;
+                let above = ((row-1) * self.width + newcol) as usize;
+
+                let rand = js_sys::Math::random();
+                let step = if rand < 0.03 {
+                    18
+                } else if rand < 0.5 {
+                    3
+                } else {
+                    0
+                };
+
+                let oldval = self.buf[4*i];
+                let newval = if oldval > step { oldval - step } else { 0 };
+                self.buf[4 * above + 0] = newval;
+                self.buf[4 * above + 1] = newval;
+                self.buf[4 * above + 2] = newval;
             }
         }
     }
 
     pub fn new() -> Canvas {
+        console_error_panic_hook::set_once();
         let width = 400u32;
         let height = 300u32;
-        let buf = vec![0; (width * height) as usize * 4];
+        let mut buf = vec![0; (width * height) as usize * 4];
+        for row in 0..height {
+            for col in 0..width {
+                let i = (row*width + col) as usize;
+                buf[4 * i + 0] = if row == height-1 && col > 125 && col < 275 { 255 } else { 0 };
+                buf[4 * i + 1] = if row == height-1 && col > 125 && col < 275 { 255 } else { 0 };
+                buf[4 * i + 2] = if row == height-1 && col > 125 && col < 275 { 255 } else { 0 };
+
+                buf[4 * i + 3] = 255;
+            }
+        }
         let t = 0;
-        Canvas { width, height, buf, t }
+        let oldt = t;
+        Canvas { width, height, buf, t, oldt}
     }
 }
